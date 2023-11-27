e72d926dbf8321de91645c4cb717e18e4c043c11
toduyemi <temit96@hotmail.com>
2023-11-27 01:49:33 -0500

    Add event to task edit button

--- src/view/baseView.js
+++ src/view/baseView.js
@@ -2,107 +2,6 @@ import { ProjectView, projectView } from './projectViewComponent';
 import { Listener } from '../helper/parent-class';
 import Menu from '../icons/menu-hamburger-nav-svgrepo-com.svg'
 import Check from '../icons/done-all-svgrepo-com.svg'
-export const baseAppHtml = {
-    body: document.body,
-    appHeader: document.createElement('header'),
-    appTitle: document.createElement('h1'),
-    sideNavSvgCtr: document.createElement('div'),
-    sideNav: document.createElement('nav'),
-    appCtr: document.createElement('div'),
-    projectsDiv: document.createElement('div'),
-    projectsUl: document.createElement('ul'),
-    homeUl: document.createElement('ul'),
-    homeDiv: document.createElement('div'),
-    addTaskBtn: document.createElement('button'),
-    addProjectBtn: document.createElement('button'),
-    displayHeader: document.createElement('h1'),
-
-    //project form html nodes
-    projectFormLi: document.createElement('li'),
-    projectTitleInput: document.createElement('input'),
-    projectForm: document.createElement('form'),
-    addButton: document.createElement('button'),
-    cancelButton: document.createElement('button'),
-
-
-
-
-    init() {
-        this.buildBaseHtml();
-        this.bindEvents();
-    },
-
-    buildBaseHtml() {
-
-        //app header bar
-        this.appHeader.innerHTML = `
-          <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-                <svg fill="#000000" width="100px" height="100px" viewBox="0 0 52 52" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M50,12.5H2a2,2,0,0,1,0-4H50a2,2,0,0,1,0,4Z"/><path d="M50,28H2a2,2,0,0,1,0-4H50a2,2,0,0,1,0,4Z"/><path d="M50,43.5H2a2,2,0,0,1,0-4H50a2,2,0,0,1,0,4Z"/></svg>
-           
-
-            <!--Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools-->
-            <svg fill="#000000" width="100px" height="100px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <g data-name="Layer 2">
-                    <g data-name="done-all">
-                        <rect width="24" height="24" opacity="0" />
-                        <path d="M16.62 6.21a1 1 0 0 0-1.41.17l-7 9-3.43-4.18a1 1 0 1 0-1.56 1.25l4.17 5.18a1 1 0 0 0 .78.37 1 1 0 0 0 .83-.38l7.83-10a1 1 0 0 0-.21-1.41z" />
-                        <path d="M21.62 6.21a1 1 0 0 0-1.41.17l-7 9-.61-.75-1.26 1.62 1.1 1.37a1 1 0 0 0 .78.37 1 1 0 0 0 .78-.38l7.83-10a1 1 0 0 0-.21-1.4z" />
-                        <path d="M8.71 13.06L10 11.44l-.2-.24a1 1 0 0 0-1.43-.2 1 1 0 0 0-.15 1.41z" />
-                    </g>
-                </g>
-            </svg>`
-
-        this.appHeader.appendChild(this.appTitle);
-
-        this.body.append(this.appHeader, this.sideNav, this.appCtr);
-
-        //sideNav
-
-
-        this.addProjectBtn.textContent = 'Add Project';
-
-
-        this.addTaskBtn.textContent = 'Add Task';
-
-        this.homeDiv.appendChild(this.homeUl);
-        this.projectsDiv.appendChild(this.projectsUl);
-        this.sideNav.append(this.homeDiv, this.projectsDiv, this.addProjectBtn);
-
-
-        this.appCtr.append(this.displayHeader, this.addTaskBtn);
-
-
-
-        //build task form but do not add to dom
-        this.projectTitleInput.setAttribute('placeholder', 'Project Title');
-        this.addButton.textContent = 'Add';
-        this.cancelButton.textContent = 'Cancel';
-        this.projectForm.append(this.projectTitleInput, this.addButton, this.cancelButton);
-        this.projectFormLi.appendChild(this.projectForm);
-
-    },
-
-    bindEvents() {
-        this.addProjectBtn.addEventListener('click', this.buildProjectForm.bind(this));
-        this.projectForm.addEventListener('submit', this.submitAddProjectEvent.bind(this));
-    },
-
-    buildProjectForm() {
-        this.projectsUl.appendChild(this.projectFormLi);
-        this.addProjectBtn.remove();
-
-    },
-
-    submitAddProjectEvent(e) {
-        e.preventDefault();
-        addProjectController();
-        this.projectsUl.removeChild(this.projectFormLi);
-        this.sideNav.appendChild(this.addProjectBtn);
-    }
-
-
-
-}
 
 
 export class AppView extends Listener {
@@ -203,13 +102,14 @@ export class AppView extends Listener {
 
     bindEvents() {
         this.addProjectBtn.addEventListener('click', this._buildProjectForm.bind(this));
-        this.addTaskBtn.addEventListener('click', this._buildAddTaskForm.bind(this))
+        this.addTaskBtn.addEventListener('click', this._buildAddTaskForm.bind(this));
+        this.tasksUl.addEventListener('click', (e) => this._buildEditProjectForm(e));
 
     }
 
     tempHtmlInit() {
 
-        this.projectForm.append(this.projectTitleInput, this.addButton, this.cancelButton);
+
 
         this.appCtr.append(this.tasksUl, this.addTaskBtn)
 
@@ -269,6 +169,7 @@ export class AppView extends Listener {
         this.homeDiv.append(this.homeUl);
         this.projectsDiv.append(this.projectsUl);
         this.sideNav.append(this.homeDiv, this.projectsDiv, this.addProjectBtn);
+        this.projectForm.append(this.projectTitleInput, this.addButton, this.cancelButton);
 
         //app or tasks
         this.appCtr = this.createElement('div', 'app-ctr');
@@ -289,6 +190,14 @@ export class AppView extends Listener {
         this.addProjectBtn.remove();
         this.sideNav.appendChild(this.projectForm);
         this.raiseChange();
+    }
+
+    _buildEditProjectForm(e) {
+
+        if (e.target.parentElement.className === 'task-edit') {
+            e.target.closest('.task-ctr').replaceChildren(this.taskForm);
+        }
+
     }
 
     _removeProjectForm() {
@@ -376,6 +285,17 @@ export class AppView extends Listener {
 
     }
 
+    bindDeleteTaskItem(handler) {
+        this.tasksUl.addEventListener('click', e => {
+            if (e.target.parentElement.className === 'task-delete') {
+                const id = e.target.closest('.task-ctr').dataset.taskId;
+                handler(id);
+                this.raiseChange();
+            }
+
+        })
+    }
+
 
     //listener function
     displayProjectList(projectsList) {
@@ -413,15 +333,6 @@ export class AppView extends Listener {
         }
     }
 
-    bindDeleteTaskItem(handler) {
-        this.tasksUl.addEventListener('click', e => {
-            if (e.target.parentElement.className === 'task-delete') {
-                const id = e.target.closest('.task-ctr').dataset.taskId;
-                handler(id);
-            }
-            this.raiseChange();
-        })
-    }
 
     //listener function
     displayTaskList(taskList) {
